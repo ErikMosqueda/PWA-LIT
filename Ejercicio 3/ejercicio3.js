f15dc1b689ecd9ce6f6c7575332da78838b95313
Erik Mosqueda <erik.19070047@itsmotul.edu.mx>
2023-02-09 11:08:18 -0600

    Ejercicio 3 de git

--- Ejercicio 3/ejercicio3.js
+++ Ejercicio 3/ejercicio3.js
@@ -2,9 +2,31 @@ import {LitElement, html} from 'lit';
 import {styles} from './styles.js';
 
 export class MotionCarousel extends LitElement {
+  static properties = {
+    selected: {type: Number},
+  };
   static styles = styles;
 
+
+  selectedInternal = 0;
+
+ constructor() {
+    super();
+    this.selected = 0;
+  }
+
+  get maxSelected() {
+    return this.childElementCount - 1;
+  }
+
+  hasValidSelected() {
+    return this.selected >= 0 && this.selected <= this.maxSelected;
+  }
+
   render() {
+    if (this.hasValidSelected()) {
+      this.selectedInternal = this.selected;
+    }
     return html`
       <div class="fit">
         <slot></slot>
