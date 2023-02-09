6ea61c9944d6c8663c0cb3c18f887ffe97d063fc
Erik Mosqueda <erik.19070047@itsmotul.edu.mx>
2023-02-09 14:45:27 -0600

    Ejercicio 5 del tutorial git

--- Ejercicio 5/converter.js
+++ Ejercicio 5/converter.js
@@ -1,11 +1,11 @@
 export const dateConverter = (locale) => {
-    return {
-      toAttribute: (date) => {
-        return date.toLocaleDateString(locale);
-      },
-      fromAttribute: (value) => {
-        return new Date(value);
-      },
-    };
+  return {
+    toAttribute: (date) => {
+      return date.toLocaleDateString(locale);
+    },
+    fromAttribute: (value) => {
+      return new Date(value);
+    },
   };
+};
   
